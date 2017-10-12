ac322af145da5c12728409a577da4d269b7b198a
Jagjit Saluja <jagjit@outlook.com.au>
2017-10-12 17:11:47 +1100

    tab spacing modified on the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
-
-    $('.nav-toggle').click(function() {
-        $('.main-nav').toggleClass('is-open');
-        $('.hamburger').toggleClass('is-open');
-    })
-
-
+  $('.nav-toggle').click(function() {
+    $('.main-nav').toggleClass('is-open');
+    $('.hamburger').toggleClass('is-open');
+  })
 })
